da29381a1dc8ce34a46e880118ae2a563b429be8
Victor-beorn <victort2938@gmail.com>
2025-04-17 19:10:09 -0300

    Atualiza projeto com novas alterações

---
+++ src/components/Input/TextInput.spec.jsx
@@ -0,0 +1,25 @@
+import { render,screen } from  '@testing-library/react';
+import {TextInput} from '.';
+import userEvent from '@testing-library/user-event';
+
+describe('<TextInput />', () => {
+    it('should have a value of searchValue', () => {
+        const fn = jest.fn();
+        render(<TextInput handleChange={fn} searchValue={'testando'}/>);
+
+        expect(screen.getByPlaceholderText(/pesquise/i)).toHaveValue('testando');
+    });
+
+    it('should call handleChance function on each key pressed', () => {
+        const fn = jest.fn();
+        render(<TextInput handleChange={fn}/>);
+
+        const input = screen.getByPlaceholderText(/pesquise/i);
+
+        const value = 'o valor do input';
+        userEvent.type(input, value);
+        expect(input.value).toBe(value);
+        expect(fn).toHaveBeenCalledTimes(value.length);
+
+    });
+});
---
+++ src/components/Posts/Post.spec.jsx
@@ -0,0 +1,47 @@
+import { render,screen } from  '@testing-library/react';
+import {Posts} from '.';
+
+const props = {
+    posts: [
+        {
+            id: 1,
+            title: 'title 1',
+            body: 'body 1 ',
+            cover: 'img/img.png',
+        },
+        {
+            id: 2,
+            title: 'title 2',
+            body: 'body 2',
+            cover: 'img/img.png',
+        },
+    ],
+};
+
+describe('<Posts />', () => {
+    it('should render post', () => {
+        render(<Posts {...props}/>);
+
+        expect(screen.getAllByRole('heading', { name: /title/i })).toHaveLength(2);
+        expect(screen.getAllByRole('img', { name: /title/i })).toHaveLength(2);
+        expect(screen.getAllByText(/body/i)).toHaveLength(2);
+        expect(screen.getAllByRole('img', { name: /title 2/i })[0]).toHaveAttribute('src', props.posts[0].cover);
+    });
+
+    it('should match snapshot', () => {
+        const {container} = render(<Posts {...props}/>);
+        expect(container.firstChild).toMatchSnapshot();
+    })
+
+    // it('should render the post content', () => {
+        
+    // });
+
+    // it('should render the post author', () => {
+        
+    // });
+
+    // it('should render the post date', () => {
+        
+    // });
+});
